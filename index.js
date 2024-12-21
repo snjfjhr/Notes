b72db7169f742fa5efcd0da465203d75f27db7a1
snjfjhr <shinjifglyaube4813@gmail.com>
2024-12-21 15:45:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ class Block{
     constructor(laneNum, delay){
         this.LaneNumber = laneNum;
         this.X = LANE_LEFTS[laneNum];
-        this.Y = - 80 * delay;
+        this.Y = - 90 * delay;
         this.Width = LANE_WIDTH;
         this.Height = BLOCK_HEIGHT;
  

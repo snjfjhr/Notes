50f6e11db8ac67664bf3cf106607095aa35f3c60
snjfjhr <shinjifglyaube4813@gmail.com>
2024-12-21 13:50:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -223,10 +223,10 @@ function gameStart(){
     // だんだん間隔を詰める
     let i=0
     while(i < 80){
-        blocks.push(new Block(1, i+8));
-        blocks.push(new Block(1, i+10));
-        blocks.push(new Block(0, i+11));
-        blocks.push(new Block(1, i+14));
+        blocks.push(new Block(1, i+16));
+        blocks.push(new Block(1, i+18));
+        blocks.push(new Block(0, i+19));
+        blocks.push(new Block(1, i+22));
         i+=8;
     }
  
